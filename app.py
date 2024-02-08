37cfbd3dbedfdf0dce5788752ebe33a9bce0f60b
basharaz1 <basharalli@h-MacBook-Pro-sl-Bashar.local>
2024-02-08 15:08:34 +0200

    save to db

--- app.py
+++ app.py
@@ -22,58 +22,18 @@ if __name__ == "__main__":
 
 
 
-from flask import Flask, request, render_template
-from flask_sqlalchemy import SQLAlchemy
-import threading
-import time
-import psutil
-
-app = Flask(__name__)
 
 
-@app.route('/')
-def homepage():
-    # global stop_cpu_monitoring
-    # global stop_memory_monitoring
-    # global stop_hd_monitoring
-    # stop_cpu_monitoring = True
-    # stop_memory_monitoring = True
-    # stop_hd_monitoring = True
-
-    cpu_cores_num = psutil.cpu_count()
-    ram_total = psutil.virtual_memory().total
-    hd_total = psutil.disk_usage('/').total
-
-    return render_template('homepage.html', cpu_cores_num=cpu_cores_num, ram_total=ram_total, hd_total=hd_total)
 
 
-@app.route('/cpu')
-def cpu_monitoring():
-    # global stop_cpu_monitoring
-    # stop_cpu_monitoring = False
-    # while not stop_cpu_monitoring:
-        cpu_percent = psutil.cpu_percent()
-        cpu_usage = psutil.cpu_usage()
-        cpu_times_usage = psutil.cpu_times_usage()
-        cpu_data = monitor_db(col_a=cpu_percent, col_b=cpu_usage[0], col_c=cpu_usage[1], col_d=col_c = cpu_usage[
-            2], col_e = col_c = cpu_usage[3], col_f = cpu_times_usage)
-        database.session.add(cpu_data)
-        database.session.commit()
 
-        cpu_db = cpu_table.query.all()
+# if __name__ == '__main__':
+#     # cpu_thread = threading.Thread(target=cpu_monitoring)
+#     # memory_thread = threading.Thread(target=memory_monitoring)
+#     # hd_thread = threading.Thread(target=hd_monitoring)
+#     #
+#     # cpu_thread.start()
+#     # memory_thread.start()
+#     # hd_thread.start()
 
-        time.sleep(600)
-    return render_template('cpu.html', cpu_db=cpu_db)
-
-
-if __name__ == '__main__':
-    init_db()
-    # cpu_thread = threading.Thread(target=cpu_monitoring)
-    # memory_thread = threading.Thread(target=memory_monitoring)
-    # hd_thread = threading.Thread(target=hd_monitoring)
-    #
-    # cpu_thread.start()
-    # memory_thread.start()
-    # hd_thread.start()
-
-    app.run()
+#     app.run()
--- views.py
+++ views.py
@@ -4,8 +4,8 @@ import psutil
 
 
 
-def homepage(is_logged_in=False,is_admin=False):
-
+def homepage():
+    collect_system_info()
     return render_template("index.html")
 
 
@@ -16,14 +16,16 @@ def homepage(is_logged_in=False,is_admin=False):
 def collect_system_info():
     memory_inf = psutil.virtual_memory()
     disk_usage = psutil.disk_usage("/")
+    cpu_times = psutil.cpu_times()
     m=Memory(total=memory_inf.total,used=memory_inf.used,active=memory_inf.active,inactive= memory_inf.inactive,usage_percent=memory_inf.percent)
     d=Disk(total=disk_usage.total,used=disk_usage.total / (1024**3),free=disk_usage.used / (1024**3),usage_percent=disk_usage.free / (1024**3))
-    
+    cpu_data = Cpu(times_user=cpu_times.user, times_system=cpu_times.system, times_idle=cpu_times.idle, usage_percent=psutil.cpu_percent(interval=1))
     db.session.add(m)
     db.session.add(d)
+    db.session.add(cpu_data)
     db.session.commit()
     
 
     
     
-    
+
